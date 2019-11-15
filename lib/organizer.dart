41d64c8123cd011bf441be47115f0bf4468ccac8
Свирин Никита Александрович <svirin@root.ians.aero>
2019-11-15 17:01:04 +0300

    Начало класса заметка

---
+++ lib/organizer.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Organizer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Organizer',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Organizer'),
+        ),
+        body: _buildNotes(),
+      ),
+    );
+  }
+
+  Widget _buildNotes() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: 1,
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+          return _buildRow("");
+        });
+  }
+
+  Widget _buildRow(String name) {
+    return ListTile(
+      title: Text(
+        name,
+      ),
+      trailing: Icon(
+        Icons.check,
+        color: Colors.green,
+      ),
+    );
+  }
+}
